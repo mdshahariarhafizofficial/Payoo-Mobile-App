804cdd0714247d2f26f7f9cce9bf7a5e15933c30
mdshahariarhafizofficial <shahariarwebdev@gmail.com>
2025-02-27 21:50:13 +0600

    transaction section added

--- script/main.js
+++ script/main.js
@@ -5,7 +5,12 @@ document.getElementById('add-money-btn').addEventListener('click', function (eve
     const pin = getInputValueById('deposit-pin');
     const accountNumber = document.getElementById('ac-no.').value;
     const mainBalance = getInnerTextById('main-balance');
+    const bank = document.getElementById('select-bank').value;
 
+    if (bank === 'Select bank') {
+        alert('Please select a bank')
+        return;
+    }
     if ( accountNumber.length === 11) {
         if ( amount <= 0 ) {
             alert('Please Enter valid amount');
@@ -19,6 +24,25 @@ document.getElementById('add-money-btn').addEventListener('click', function (eve
             const sum = mainBalance+amount;
             setInnerTextByIdAndValue('main-balance', sum);
             
+            const container = document.getElementById('transaction-container');
+            const div = document.createElement('div');
+            div.innerHTML = `
+                        <div class="flex justify-between items-center border border-gray-300 bg-white p-3 mb-4 rounded-md">
+                            <div class="flex gap-4 items-center">
+                                <div>
+                                    <img src="asset/wallet 1.png" alt="">
+                                </div>
+                                <div>
+                                    <p class="text-sm font-extrabold text-blue-500">${bank}</p>
+                                    <h2 class="text-2xl font-extrabold">$ ${amount}</h2>
+                                </div>
+                            </div>
+                            <p class="text-sm font-medium text-center">Add money</p>
+                            <p class="text-xl font-extrabold text-blue-500">AC: ${accountNumber}</p>
+                        </div>
+            `
+            container.appendChild(div);
+
         }
         else{
             alert('Enter valid pin number')
@@ -38,6 +62,7 @@ document.getElementById('withdraw-btn').addEventListener('click', function(event
     const cashOutPin = getInputValueById('cash-out-pin');
     const mainBalance = getInnerTextById('main-balance');
 
+
     if ( agentNumber.length === 11) {
         if (cashOutAmount <= 0) {
             alert('Enter Valid Amount');
@@ -54,6 +79,25 @@ document.getElementById('withdraw-btn').addEventListener('click', function(event
             else{
                 const sub = mainBalance-cashOutAmount;
                 setInnerTextByIdAndValue('main-balance', sub);
+
+                const container = document.getElementById('transaction-container');
+                const div = document.createElement('div');
+                div.innerHTML = `
+                            <div class="flex justify-between items-center border border-gray-300 bg-white p-3 mb-4 rounded-md">
+                                <div class="flex gap-4 items-center">
+                                    <div>
+                                        <img src="asset/wallet 1.png" alt="">
+                                    </div>
+                                    <div>
+                                        <p class="text-sm font-extrabold text-blue-500"></p>
+                                        <h2 class="text-2xl font-extrabold">$ ${cashOutAmount}</h2>
+                                    </div>
+                                </div>
+                                <p class="text-sm font-medium text-center">Cash Out</p>
+                                <p class="text-xl font-extrabold text-blue-500">AC: ${agentNumber}</p>
+                            </div>
+                `
+                container.appendChild(div);
             }
         }else{
             alert('Enter Valid PIN Number') 
@@ -66,6 +110,7 @@ document.getElementById('withdraw-btn').addEventListener('click', function(event
 
 // Toggle Function
 document.getElementById('cash-out-section').style.display = 'none';
+document.getElementById('transaction-section').style.display = 'none';
 
 document.getElementById('add-money-card').addEventListener( 'click', function(){
     toggle('cash-out-section', 'none');
